96b321df392368d8ee899038d5000dbdd5928e4f
Adrian Zimmermann <zadrian@web.de>
2020-11-25 00:43:11 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# GfxMath
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import GfxMathTests
+
+var tests = [XCTestCaseEntry]()
+tests += GfxMathTests.allTests()
+XCTMain(tests)
