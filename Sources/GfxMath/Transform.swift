9746c737354bb79acbed49dbd3effea3f59cd5ef
UnGast <zadrian@web.de>
2021-02-16 11:04:00 +0100

    apply transforms in reverse direction, apply transforms to size

--- Sources/GfxMath/Transform.swift
+++ Sources/GfxMath/Transform.swift
@@ -18,17 +18,37 @@ public enum DTransform2 {
       return result
     }
   }
+
+  public func transform(size: DSize2) -> DSize2 {
+    switch self {
+    case let .translate(translation):
+      return size
+    case let .scale(scale, origin):
+      return size * scale.abs()
+    }
+  }
 }
 
 extension Array where Element == DTransform2 {
+  /** applies the transforms in reverse order */
   public func transform(point: DVec2) -> DVec2 {
     var result = point
-    for transform in self {
+    for transform in self.reversed() {
       result = transform.transform(point: result)
     }
     return result
   }
 
+  /** applies the transforms in reverse order */
+  public func transform(size: DSize2) -> DSize2 {
+    var result = size
+    for transform in self.reversed() {
+      result = transform.transform(size: result)
+    }
+    return result
+  }
+
+  /** applies the transforms in reverse order */
   public func transform(rect: DRect) -> DRect {
     DRect(min: transform(point: rect.min), max: transform(point: rect.max))
   }
