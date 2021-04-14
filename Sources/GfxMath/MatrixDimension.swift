afec80f72e254d82680e90061b67e18f493a861a
Adrian Zimmermann <zadrian@web.de>
2021-04-14 23:43:27 +0200

    flesh out MatrixDimension

--- Sources/GfxMath/MatrixDimension.swift
+++ Sources/GfxMath/MatrixDimension.swift
@@ -1,7 +1,54 @@
-public protocol MatrixDimension {}
+public protocol MatrixDimension {
+  var rows: Int { get }
+  var cols: Int { get }
+}
 
-public struct Dim_2x1: MatrixDimension {}
-public struct Dim_3x1: MatrixDimension {}
-public struct Dim_3x3: MatrixDimension {}
-public struct Dim_4x1: MatrixDimension {}
-public struct Dim_nx1: MatrixDimension {}
+extension MatrixDimension {
+  public var itemCount: Int {
+    rows * cols
+  }
+}
+
+public protocol StaticMatrixDimension: MatrixDimension {
+  static var rows: Int { get }
+  static var cols: Int { get }
+}
+
+extension StaticMatrixDimension {
+  public static var itemCount: Int { 
+    rows * cols
+  }
+
+  public var rows: Int {
+    Self.rows
+  }
+
+  public var cols: Int {
+    Self.cols
+  }
+}
+
+public struct Dim_2x1: StaticMatrixDimension {
+  public static let rows = 2
+  public static let cols = 2
+}
+public struct Dim_3x1: StaticMatrixDimension {
+  public static let rows = 3
+  public static let cols = 1
+}
+public struct Dim_3x3: StaticMatrixDimension {
+  public static let rows = 3
+  public static let cols = 3
+}
+public struct Dim_4x1: StaticMatrixDimension {
+  public static let rows = 4
+  public static let cols = 1
+}
+public struct Dim_nx1: MatrixDimension {
+  public let rows: Int
+  public let cols: Int = 1
+
+  public init(rows: Int) {
+    self.rows = rows
+  }
+}
