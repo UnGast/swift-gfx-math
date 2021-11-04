871f4eb1e95f455f89760d4aaf551bb3d17aa582
Adrian Zimmermann <zadrian@web.de>
2021-11-04 11:19:44 +0100

    fix swim conversion

--- Sources/GfxMath/Color/Experimental/Conversions/EColor+swim.swift
+++ Sources/GfxMath/Color/Experimental/Conversions/EColor+swim.swift
@@ -1,7 +1,7 @@
 #if canImport(Swim)
 import Swim 
 
-extension ERGBColor {
+extension ERGBColor where Self.DataType: Swim.DataType {
   public var swim: Swim.Color<RGB, DataType> {
     Swim.Color(r: r, g: g, b: b)
   }
